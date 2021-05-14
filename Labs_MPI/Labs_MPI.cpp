8faf9c5551a81ae8b459e640e357fed857c415e4
Termitsforge <medic573@gmail.com>
2021-05-14 22:47:24 +0300

    Lab3 may be done

--- Labs_MPI/Labs_MPI.cpp
+++ Labs_MPI/Labs_MPI.cpp
@@ -6,72 +6,66 @@ using namespace std;
 
 int main(int *argc, char* argv[])
 {
-	int ProcNum, ProcRank, ProcSum;
-	int rbuf[100], * sendbuf;
+	int ProcNum, ProcRank, ProcSum, result = 0;
+	int rbuf[100], * sendbuf, *resultbuf;
 	// <программный код без использования MPI функций>
 	MPI_Init(argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
 	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+	resultbuf = (int*)malloc(ProcNum * 100 * sizeof(int));
+	sendbuf = (int*)malloc(ProcNum * 100 * sizeof(int));
 	if (ProcRank == 0) {
 		for (int i = 0; i < 100; i++) {
-			rbuf[i] = i + 10;
+			sendbuf[i] = i;
 		}
 	}
-	sendbuf = (int*)malloc(ProcNum * 100 * sizeof(int));
-	MPI_Scatter(sendbuf, 100, MPI_INT, rbuf, 100, MPI_INT, 0, MPI_COMM_WORLD);
-	if (ProcRank != 0) {
-		printf("---------------------------------------------------------\n");
-		printf("rank: %d;\n", ProcRank);
-		for (int i = 0; i < 100; i++) {
-			rbuf[i] = i + 10;
-			printf("i: %d; sendbuf: %d\n", i, *sendbuf);
-		}
-		printf("---------------------------------------------------------\n");
-	}
-	
-
-
-
-
-
-	/*switch (ProcRank) {
-		case 1:
-			printf("rank: %d;\n", ProcRank);
-			for (int i = 0; i < 25; i++) {
-				rbuf[i] = i + 10;
-				printf("i: %d; rbuf[i]: %d\n", i, rbuf[i]);
-			}
-			break;
-		case 2:
-			printf("rank: %d;\n", ProcRank);
-			for (int i = 25; i < 50; i++) {
-				rbuf[i] = i + 10;
-				printf("i: %d; rbuf[i]: %d\n", i, rbuf[i]);
-			}
-			break;
-			printf("rank: %d;\n", ProcRank);
-		case 3:
-			for (int i = 50; i < 75; i++) {
-				rbuf[i] = i + 10;
-				printf("i: %d; rbuf[i]: %d\n", i, rbuf[i]);
-			}
-			break;
-	}
-	
-	*/
-	/*if(!ProcRank == 0){
-		printf("rank: %d;\n", ProcRank);
+	MPI_Scatter(sendbuf, 25, MPI_INT, rbuf, 25, MPI_INT, 0, MPI_COMM_WORLD);
+	switch (ProcRank) {
+	case 0:
+		//printf("rank: %d;\n", ProcRank);
 		for (int i = 0; i < 25; i++) {
-			rbuf[i] = i + 10;
-			printf("i: %d; rbuf[i]: %d\n",i, rbuf[i]);
+			rbuf[i] += ProcRank;
+			//printf("i: %d; rbuf[i]: %d\n", i, rbuf[i]);
 		}
-	}*/
+		break;
+	case 1:
+		//printf("rank: %d;\n", ProcRank);
+		for (int i = 0; i < 25; i++) {
+			rbuf[i] += ProcRank;
+			//printf("i: %d; rbuf[i]: %d\n", i, rbuf[i]);
+		}
+		break;
+	case 2:
+		//printf("rank: %d;\n", ProcRank);
+		for (int i = 0; i < 25; i++) {
+			rbuf[i] += ProcRank;
+			//printf("i: %d; rbuf[i]: %d\n", i, rbuf[i]);
+		}
+		break;
+	case 3:
+		//printf("rank: %d;\n", ProcRank);
+		for (int i = 0; i < 25; i++) {
+			rbuf[i] += ProcRank;
+			//printf("i: %d; rbuf[i]: %d\n", i, rbuf[i]);
+		}
+		break;
+	}
+
+	MPI_Gather(rbuf, 25, MPI_INT, resultbuf, 25, MPI_INT, 0, MPI_COMM_WORLD);
+
+	if (ProcRank == 0) {
+		printf("------------------------------\n");
+		for (int i = 0; i < 50; i++) {
+			printf("i: %d; resultbuf[i]: %d \t i: %d; resultbuf[i]: %d\n", i, resultbuf[i], i+50, resultbuf[i+50]);
+			if (i == 24 || i == 49 || i == 74 || i == 99) {
+				printf("-------------\n");
+			}
+		}
+	}
+
 	// <программный код с использованием MPI функций>
 	MPI_Finalize();
-	/*printf("without MPI\n");
-	for (int i = 0; i < 99; i++) {
-		printf("i: %d; rbuf[i]: %d\n", i, rbuf[i]);
-	}*/
+	
 	// <программный код без использования MPI функций>
 	return 0;
 }
